a8980227426eec00d077e29c6aad65684c41ed07
Anton Parfonov <antonparfonov@gmail.com>
2024-04-17 23:39:22 +0300

    Fix docker port exposure

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -29,7 +29,7 @@ docker run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
+    -p 127.0.0.1:"${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
 fi
